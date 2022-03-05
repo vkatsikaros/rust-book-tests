882827b40f20bf7c09f341a947ddd530f8d2b568
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-05 09:44:36 +0200

    Cannot have a mutable reference while we have an immutable one to the same value
    Multiple immutable references are allowed, this cannot cause a data
    race problem

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ fn main() {
     // one mutable reference to a particular piece of data at a time. 
     let mut s1 = String::from("Hello");
 
-    {
-        // create one mutable reference
-        let r1 = &mut s1;
-    } // r1 goes out of scope
-    let r2 = &mut s1;
+    let r1 = &s1;
+    let r2 = &s1;
+    let r3 = &mut s1;
+
+    println!("{}, {}, {}", r1, r2, r3);
 }
