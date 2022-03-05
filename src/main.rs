6e502d9e6c407fcdbbb831cd6e32e47b33412a91
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-05 09:48:54 +0200

    Example of immutable references going out of scope allowing a mutable reference

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,17 @@ fn main() {
 
     let r1 = &s1;
     let r2 = &s1;
-    let r3 = &mut s1;
+    println!("{}, {}", r1, r2);
+    // r1, r2 are not used after this (their scope ends)
 
-    println!("{}, {}, {}", r1, r2, r3);
+    // the mutable reference is now allowed. In the scope there are no
+    // other immutable references
+    let r3 = &mut s1;
+    println!("{}", r3);
+
+    // of course if this code was used, the scope of the immutabel r1, r2
+    // would overlap with the scope of the mutable reference, so we would have
+    // error[E0502]: cannot borrow `s1` as mutable because it is also borrowed as immutable
+    //
+    //println!("{}, {}", r1, r2);
 }
