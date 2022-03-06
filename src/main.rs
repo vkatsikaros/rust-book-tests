ef8d18bf5108742bfa43af4ea1b7012a145410a5
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-06 18:56:33 +0200

    Add slice

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
-    let ref_to_nothing = dangle();
+
 }
 
-fn dangle() -> &String {
-    let s = String::from("Hello");
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
 
-    &s
-} // s goes out of scope and is freed. The reference returned is dangling
-// ie, a pointer that references a location in memory that may have been given to someone else-
+    for(i, &item) in bytes.iter().enumerate(){
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
