9f02fdfbadd0021d288f82ea9801a708d0bbe8b8
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-06 18:45:56 +0200

    Cause error with dangling reference
    error[E0106]: missing lifetime specifier
     --> src/main.rs:5:16
      |
    5 | fn dangle() -> &String {
      |                ^ expected named lifetime parameter
      |
      = help: this function's return type contains a borrowed value, but there is no value for it to be borrowed from
    help: consider using the `'static` lifetime
      |
    5 | fn dangle() -> &'static String {
      |                ~~~~~~~~
    
    For more information about this error, try `rustc --explain E0106`.
    error: could not compile `testbook` due to previous error

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
 fn main() {
-    // Mutable references have one big restriction: you can have only
-    // one mutable reference to a particular piece of data at a time. 
-    let mut s1 = String::from("Hello");
-
-    let r1 = &s1;
-    let r2 = &s1;
-    println!("{}, {}", r1, r2);
-    // r1, r2 are not used after this (their scope ends)
-
-    // the mutable reference is now allowed. In the scope there are no
-    // other immutable references
-    let r3 = &mut s1;
-    println!("{}", r3);
-
-    // of course if this code was used, the scope of the immutabel r1, r2
-    // would overlap with the scope of the mutable reference, so we would have
-    // error[E0502]: cannot borrow `s1` as mutable because it is also borrowed as immutable
-    //
-    //println!("{}, {}", r1, r2);
+    let ref_to_nothing = dangle();
 }
+
+fn dangle() -> &String {
+    let s = String::from("Hello");
+
+    &s
+} // s goes out of scope and is freed. The reference returned is dangling
+// ie, a pointer that references a location in memory that may have been given to someone else-
