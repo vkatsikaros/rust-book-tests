f740df436f341ecb9616719a0d2fde744e24edc5
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-06 18:57:30 +0200

    Add and use a struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-fn main() {
-
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
 
-fn first_word(s: &String) -> &str {
-    let bytes = s.as_bytes();
+fn main() {
+    let u1 = build_user( String::from("alice@example.com"), String::from("Alice") );
+    println!("{}", u1.username);
+}
 
-    for(i, &item) in bytes.iter().enumerate(){
-        if item == b' ' {
-            return &s[0..i];
-        }
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
     }
-    &s[..]
 }
