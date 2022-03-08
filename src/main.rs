c073f7c28038b3bbc7dc60bf52b7dccce53aa12e
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-08 08:45:28 +0200

    structs can store references to data owned by something else
    This requires the use of 'lifetimes' We will NOT cover this right now
    
    The error is:
    error[E0106]: missing lifetime specifier
     --> src/main.rs:3:15
      |
    3 |     username: &str,
      |               ^ expected named lifetime parameter
      |
    help: consider introducing a named lifetime parameter
      |
    1 ~ struct User<'a> {
    2 |     active: bool,
    3 ~     username: &'a str,
      |
    
    error[E0106]: missing lifetime specifier
     --> src/main.rs:4:12
      |
    4 |     email: &str,
      |            ^ expected named lifetime parameter
      |
    help: consider introducing a named lifetime parameter
      |
    1 ~ struct User<'a> {
    2 |     active: bool,
    3 |     username: &str,
    4 ~     email: &'a str,
      |
    
    For more information about this error, try `rustc --explain E0106`.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-struct SomeConditionFoo;
+struct User {
+    active: bool,
+    username: &str,
+    email: &str,
+    sign_in_count: u64,
+}
 
 fn main() {
-    let a = SomeConditionFoo;
+    let user1 = User {
+        email: "alice@example.com",
+        username: "alice",
+        active: true,
+        sign_in_count: 1,
+    };
 }
