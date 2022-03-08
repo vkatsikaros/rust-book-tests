f2adbb1d88d1378ebe972257b64075a0b6bec932
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-08 09:23:17 +0200

    Example with structs and function takes ownership (value moves into fn)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-fn main() {
-    let height = 50;
-    let width = 10;
+struct Rectangle {
+    height: u32,
+    width: u32,
+}
 
-    let area = area(width, height);
+fn main() {
+    let r1 = Rectangle {
+        width: 50,
+        height: 10,
+    };
+
+    let area = area(r1);
     println!("Area {}", area);
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(r: Rectangle) -> u32 {
+    r.width * r.height
 }
