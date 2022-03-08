fc4d4744c37f3b12866a511fb44bb63623aae23c
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-08 08:52:01 +0200

    Example without structs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-struct User {
-    active: bool,
-    username: &str,
-    email: &str,
-    sign_in_count: u64,
+fn main() {
+    let height = 50;
+    let width = 10;
+
+    let area = area(width, height);
+    println!("Area {}", area);
 }
 
-fn main() {
-    let user1 = User {
-        email: "alice@example.com",
-        username: "alice",
-        active: true,
-        sign_in_count: 1,
-    };
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
