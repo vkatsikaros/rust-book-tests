7a9912b6058ada5f7e6e38708a7d46c96940252d
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-10 07:41:56 +0200

    Use a method insted of a function
    Methods are like fucntions. However, they are implemented in the scope
    of a struct, enum or trait.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-#[derive(Debug)]
 struct Rectangle {
     height: u32,
     width: u32,
 }
 
+impl Rectangle{
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let i = 3;
     let r1 = Rectangle {
-        width: dbg!(50 * i),
+        width: 50,
         height: 10,
     };
 
-    let area = area(&r1);
-    dbg!(&r1);
-}
-
-fn area(r: &Rectangle) -> u32 {
-    r.width * r.height
+    let area = r1.area();
+    println!("{}", area);
 }
