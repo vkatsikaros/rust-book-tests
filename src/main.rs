c68fcbe1d6d93c3924fba988a66fbf7f5d3820f8
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-10 09:29:35 +0200

    Define a simple enum

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,11 @@
 #[derive(Debug)]
-struct Rectangle {
-    height: u32,
-    width: u32,
-}
-
-impl Rectangle{
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn square(size: u32) -> Rectangle {
-        Rectangle {
-            width: size,
-            height: size,
-        }
-    }
+enum IpAddrKind {
+    V4,
+    V6
 }
 
 fn main() {
-    let r1 = Rectangle::square(10);
-    dbg!(r1);
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    dbg!(four);
 }
