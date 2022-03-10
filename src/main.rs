b88d639aefadfbddfa088a15c38fd29aa15aa439
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-10 07:58:20 +0200

    Use method with more than 1 parameters

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ impl Rectangle{
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width 
+        && self.height > other.height
+    }
 }
 
 fn main() {
@@ -15,6 +20,14 @@ fn main() {
         height: 10,
     };
 
-    let area = r1.area();
-    println!("{}", area);
+    let r2 = Rectangle {
+        width: 60,
+        height: 20,
+    };
+
+    let mut i = r1.can_hold(&r2);
+    println!("{}", i);
+
+    i = r2.can_hold(&r1);
+    println!("{}", i);
 }
