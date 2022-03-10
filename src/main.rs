6b207cdb4c50cff07380aca807541c7663b11046
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-10 21:25:31 +0200

    Introduce match

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny   => 1,
+        Coin::Nickel  => 5,
+        Coin::Dime    => 10,
+        Coin::Quarter => 25,
+    }
+}
+
 fn main() {
-    let x: u8 = 5;
-    let y: Option<u8> = Some(5);
-    
-    let sum = x + y;
+    let x = Coin::Dime;
+    let cents = value_in_cents(x);
+    println!("{}", cents);
 }
