245393c3dfe298a54249aa46f1b7f8d3dbf8c9fc
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-10 09:32:15 +0200

    Define an enum and a struct field that is using this enum

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,16 @@ enum IpAddrKind {
     V6
 }
 
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-    dbg!(four);
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    dbg!(&home);
 }
