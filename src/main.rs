ae5daeb1595891a64412775598c99f7138c5f321
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-10 09:54:37 +0200

    Introduce Options. We used an enum std::option::Option
    However, Option can also be `None`. rust wants to ensure that the values
    always get the correct type. As the book says:
      You have to convert an Option<T> to a T
      before you can perform T operations with it.
    
    error[E0277]: cannot add `Option<u8>` to `u8`
     --> src/main.rs:5:17
      |
    5 |     let sum = x + y;
      |                 ^ no implementation for `u8 + Option<u8>`
      |
      = help: the trait `Add<Option<u8>>` is not implemented for `u8`
    
    For more information about this error, try `rustc --explain E0277`.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
-#[derive(Debug)]
-enum IpAddr {
-    V4(u8,u8,u8,u8),
-    V6(String),
-}
-
 fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loopback = IpAddr::V6(String::from("::1"));
-    dbg!(&home);
-    dbg!(&loopback);
+    let x: u8 = 5;
+    let y: Option<u8> = Some(5);
+    
+    let sum = x + y;
 }
