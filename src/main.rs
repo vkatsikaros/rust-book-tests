627c29780e15b7ee10be32c610461400705d7c5f
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-11 09:16:22 +0200

    Use match arm with a final catch all expression
    If we place a matching expression after the catch-all rust warns about
    it
    
    PS book uses `other` for the catch-all expression. However, this does
    not seem to be a keyword, it simply looks like a variable name?
    
    warning: unreachable pattern
      --> src/main.rs:10:9
       |
    9  |         foooo => move_player(), // book says `other`, but is `other` special
       |         ----- matches any value
    10 |         5 => remove_hat(),
       |         ^ unreachable pattern
       |
       = note: `#[warn(unreachable_patterns)]` on by default
    
    warning: unused variable: `foooo`

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 fn main() {
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
-}
+    let dice_roll = 9;
 
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        Some(i) => Some(i +1),
+    match dice_roll {
+        3 => add_hat(),
+        4 => remove_hat(),
+        // we have not listed all possible values. However, the last
+        // arm is a catch-all
+        foooo => move_player(), // book says `other`, but is `other` special
+        5 => remove_hat(),
     }
 }
+
+fn add_hat() {}
+fn remove_hat() {}
+fn move_player() {}
