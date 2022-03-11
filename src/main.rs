9a9b63183f07a877c9ad823ecbddbb5c0821cdec
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-11 10:07:04 +0200

    Add a match where we want to focus only on 1 arm (we'll change that later)

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,8 @@
-//
-// https://doc.rust-lang.org/reference/patterns.html#patterns
-//
-// Patterns are used to match values against structures 
-// and to, optionally, bind variables to values inside 
-// these structures. They are also used in variable 
-// declarations and parameters for functions and closures.
-
 fn main() {
-    let dice_roll = 9;
+    let config_max = Some(3u8);
 
-    match dice_roll {
-        3 => add_hat(),
-        4 => remove_hat(),
-        // Wildcard pattern
-        // https://doc.rust-lang.org/reference/patterns.html#wildcard-pattern
-        _ => move_player(),
+    match config_max {
+        Some(max) => println!("max is {}", max),
+        _ => (),
     }
 }
-
-fn add_hat() {}
-fn remove_hat() {}
-fn move_player() {}
