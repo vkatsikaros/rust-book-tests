0ddf27ab31d95f72073fa57e953fce360c0ae783
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-11 09:26:54 +0200

    Use match with a wildchar pattern to match any value
    https://doc.rust-lang.org/reference/patterns.html#wildcard-pattern
    Referene doc says
      It is used to ignore values when they don't matter.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
+//
+// https://doc.rust-lang.org/reference/patterns.html#patterns
+//
+// Patterns are used to match values against structures 
+// and to, optionally, bind variables to values inside 
+// these structures. They are also used in variable 
+// declarations and parameters for functions and closures.
+
 fn main() {
     let dice_roll = 9;
 
     match dice_roll {
         3 => add_hat(),
         4 => remove_hat(),
-        // we have not listed all possible values. However, the last
-        // arm is a catch-all
-        foooo => move_player(), // book says `other`, but is `other` special
-        5 => remove_hat(),
+        // Wildcard pattern
+        // https://doc.rust-lang.org/reference/patterns.html#wildcard-pattern
+        _ => move_player(),
     }
 }
 
