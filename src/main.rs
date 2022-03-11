818bc04d93e7dd5a40277a5f55e6cd3cc7de86c5
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-11 09:06:47 +0200

    Matching with Option<T>

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,12 @@
-#[derive(Debug)]
-enum Fruit {
-    Apple,
-    Banana,
-}
-
-#[derive(Debug)]
-enum Coin {
-    Penny,
-    Nickel,
-    Dime(Fruit),
-    Quarter,
-}
-
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny   => 1,
-        Coin::Nickel  => 5,
-        Coin::Dime(state)    => {
-            println!("Dime with {:?}", state);
-            10
-        },
-        Coin::Quarter => 25,
-    }
-}
-
 fn main() {
-    let x = Coin::Dime(Fruit::Apple);
-    let cents = value_in_cents(x);
-    println!("{}", cents);
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i +1),
+    }
 }
