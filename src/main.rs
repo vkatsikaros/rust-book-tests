8f64ca7ff447b30cbc31c22476268d66cb74b0c4
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-11 10:08:28 +0200

    Switch from a `match` to an `if let`

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 fn main() {
     let config_max = Some(3u8);
 
-    match config_max {
-        Some(max) => println!("max is {}", max),
-        _ => (),
+    if let Some(max) = config_max {
+        println!("max is {}", max);
     }
 }
