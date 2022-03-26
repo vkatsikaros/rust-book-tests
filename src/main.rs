e1d702c451e1f791a0636f1cbaa1a7a81ea33808
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-26 13:28:55 +0200

    Read out of bounds from vector

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
+
 fn main() {
     let v = vec![1,2,3,4,5];
 
-    let t: &i32 = &v[2];
-    println!("{}", t);
+    // reading from out of bounds will crash the program
+    //let x = &v[100];
 
-    match v.get(2) {
-        // need to come back and understand why we can put a variable in
-        // the Some() that is not the match expression
-        Some(t) => println!("3rd is: {}", t),
-        None => println!("no 3rd element"),
-    }
+    // reading from out of bounds returns None
+    let x = v.get(100);
+    dbg!(x);
+
 }
