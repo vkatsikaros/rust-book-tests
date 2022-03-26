2caa851f4f0698072e940ae9e093ecfe420f4043
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-26 13:23:25 +0200

    Read from vector

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
-    let mut v = Vec::new();
+    let v = vec![1,2,3,4,5];
 
-    v.push(5);
+    let t: &i32 = &v[2];
+    println!("{}", t);
+
+    match v.get(2) {
+        // need to come back and understand why we can put a variable in
+        // the Some() that is not the match expression
+        Some(t) => println!("3rd is: {}", t),
+        None => println!("no 3rd element"),
+    }
 }
