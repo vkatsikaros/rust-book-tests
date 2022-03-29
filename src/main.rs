55835bd34747b7178bc745b30420e55d37be3ff8
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-29 10:31:28 +0300

    vector: borrow an immutable error
    the reason is that adding an element to the vector might require
    re-allocation of the vector. So, the reference might point to
    deallocated memory.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-
 fn main() {
-    let v = vec![1,2,3,4,5];
+    let mut v = vec![1,2,3,4,5];
 
-    // reading from out of bounds will crash the program
-    //let x = &v[100];
-
-    // reading from out of bounds returns None
-    let x = v.get(100);
-    dbg!(x);
-
+    let f = &v[0];
+    v.push(6);
+    println!("{}", f);
 }
