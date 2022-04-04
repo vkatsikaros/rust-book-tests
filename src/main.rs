79d92d76b167569676ae11bfa3d1b4f5da01914e
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-04-04 08:24:27 +0300

    Explain string operator +
    https://doc.rust-lang.org/std/string/struct.String.html#trait-implementations

--- src/main.rs
+++ src/main.rs
@@ -21,4 +21,16 @@ fn main() {
     let mut s5 = String::from("foo");
     s5.push_str("bar");
     println!("{}", s5);
+
+    let s6 = String::from("foo");
+    let s7 = String::from("bar");
+    let space = String::from(" ");
+    let fb = s6 + &space + &s7;
+    // + (or `add`) takes ownership of the take ownership of 1st argument, s6
+    // the rest of the arguments are references, whose contents are copied, space and s7
+    // and then returns ownership of the results
+    // So... s6 is not valid afterwards
+    println!("{}", fb);
+
+    let fb2 = format!("{} {}", s6, s6); // s6 is not valid
 }
